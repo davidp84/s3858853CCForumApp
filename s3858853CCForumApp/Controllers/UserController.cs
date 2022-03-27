69e87e1960e9dc68256781defbbbfbf7c613b118
davidp84 <62162575+davidp84@users.noreply.github.com>
2022-03-27 11:29:40 +1100

    draft forum and user pages

---
+++ s3858853CCForumApp/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace s3858853CCForumApp.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
