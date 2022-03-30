3c6d15f6a19ec27526d2ad43b53e33e3e85ff15e
davidp84 <62162575+davidp84@users.noreply.github.com>
2022-03-30 19:48:03 +1100

    Added authorised user class

---
+++ s3858853CCForumApp/Authorisation/AuthorisedUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using s3858853CCForumApp.Models;
+
+namespace s3858853CCForumApp.Attributes
+{
+    public class AuthorisedUserAttribute : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var customerID = context.HttpContext.Session.GetInt32(nameof(User.id));
+            if (!customerID.HasValue)
+            {
+                context.Result = new RedirectToActionResult("Index", "Home", null);
+            }
+        }
+    }
+}
