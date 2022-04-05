89e4a5744759edeac146531965779bbdad626496
davidp84 <62162575+davidp84@users.noreply.github.com>
2022-04-05 11:24:52 +1000

    Tidy up forum display and new forum post

--- s3858853CCForumApp/Controllers/ForumController.cs
+++ s3858853CCForumApp/Controllers/ForumController.cs
@@ -25,28 +25,6 @@ namespace s3858853CCForumApp.Controllers
         {
         }
 
-        public IActionResult Index()
-        {
-            DatastoreDb _context = new DatastoreDbBuilder
-            {
-                ProjectId = "s3858853-a1",
-                EmulatorDetection = EmulatorDetection.EmulatorOrProduction
-            }.Build();
-
-            KeyFactory _keyFactory = _context.CreateKeyFactory("user");
-
-            Key key = _keyFactory.CreateKey("default");
-
-            Query query = new Query("user")
-            {
-                Filter = Filter.Equal("id", UserID)
-            };
-
-            //lazy loading
-            var user = _context.RunQueryLazilyAsync(query);
-            return View(user);
-        }
-
         public async Task<IActionResult> Forum(int id)
         {
             DatastoreDb _context = new DatastoreDbBuilder
@@ -66,6 +44,55 @@ namespace s3858853CCForumApp.Controllers
 
             //lazy loading
             var user = _context.RunQueryLazilyAsync(query);
+
+            var tempUser = new User();
+
+            await user.ForEachAsync(x =>
+            {
+                
+                queryUser = (string)x["id"];
+
+                tempUser.id = (string)x["id"];
+                tempUser.user_name = (string)x["user_name"];
+                tempUser.password = (string)x["password"];
+                tempUser.image = (string)x["image"];
+
+            });
+
+            ViewBag.user = tempUser;
+
+            KeyFactory _newKeyFactory = _context.CreateKeyFactory("post");
+
+            Key newKey = _keyFactory.CreateKey("default");
+
+            Query secondQuery = new Query("Post")
+            {
+                Limit = 10,
+                Order = { { "created", PropertyOrder.Types.Direction.Descending } }
+            };
+
+            //lazy loading
+            var posts = _context.RunQueryLazilyAsync(secondQuery);
+
+            var tempPost = new Post();
+
+            var userPosts = new List<Post>();
+
+            await posts.ForEachAsync(x =>
+            {
+                tempPost.subject = (string)x["subject"];
+                tempPost.UserID = (string)x["UserID"];
+                tempPost.messageText = (string)x["messageText"];
+                tempPost.postTimeUTC = (string)x["postTimeUTC"];
+                tempPost.Image = (string)x["Image"];
+
+                userPosts.Add(tempPost);
+
+            });
+
+            return View(userPosts);
+
+
             return View(user);
         }
 
@@ -91,7 +118,7 @@ namespace s3858853CCForumApp.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> NewPost(string subject, string messageText, string imageName, IFormFile image, string imagePath)
+        public async Task<IActionResult> NewPost(string subject, string messageText, IFormFile image)
         {
             DatastoreDb _context = new DatastoreDbBuilder
             {
@@ -101,6 +128,8 @@ namespace s3858853CCForumApp.Controllers
 
             KeyFactory keyFactory = _context.CreateKeyFactory("post");
 
+             var imageName = Image.FileName;
+
             string imageString = "gs://s3858853-a1-storage/" + imageName;
 
             // Prepare bucket and image for upload
@@ -109,17 +138,22 @@ namespace s3858853CCForumApp.Controllers
 
             var bucket = client.GetBucketAsync("s3858853-a1-storage");
 
-            var content = new FileStream(imagePath, FileMode.Open, FileAccess.Read);
+            var obj1 = "a";
 
-
-            // Upload file to bucket
             if (imageName.Contains(".png"))
             {
-                var obj1 = client.UploadObjectAsync("s3858853-a1-storage", imageName, "image/png", content);
+                obj1 = "image/png";
             }
             else
             {
-                var obj1 = client.UploadObjectAsync("s3858853-a1-storage", imageName, "image/jpg", content);
+                obj1 = "image/jpg";
+            }
+
+            // Upload file to bucket
+            using (var memoryStream = new MemoryStream())
+            {
+                await Image.CopyToAsync(memoryStream);
+                var dataObject = await client.UploadObjectAsync("s3858853-a1-storage", imageName, obj1, memoryStream);                
             }
 
             Entity update = new Entity
@@ -133,7 +167,7 @@ namespace s3858853CCForumApp.Controllers
             };
             await _context.InsertAsync(update);
 
-            return RedirectToAction("Forum", "User");
+            return RedirectToAction("Forum", "Forum");
 
         }
     }
